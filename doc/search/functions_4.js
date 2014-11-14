97332625f782ebafbac6478145eedf8891c1539f
Tong Guo <Tong-G@outlook.com>
2014-11-15 04:16:53 +0800

    UPDATED the documentation.

--- doc/search/functions_4.js
+++ doc/search/functions_4.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['unobserve_3a',['unobserve:',['../interface_f_b_k_v_o_controller.html#ae614819487de82c3a513610031834a6d',1,'FBKVOController']]],
-  ['unobserve_3ainfo_3a',['unobserve:info:',['../interface___f_b_k_v_o_shared_controller.html#a8c1ecb634733652d00e1d0171eea4856',1,'_FBKVOSharedController']]],
-  ['unobserve_3ainfos_3a',['unobserve:infos:',['../interface___f_b_k_v_o_shared_controller.html#a672c0e721f004e5f34ea8e497d9f4841',1,'_FBKVOSharedController']]],
-  ['unobserve_3akeypath_3a',['unobserve:keyPath:',['../interface_f_b_k_v_o_controller.html#aebaeb37fe373939c8254984bdf3c518a',1,'FBKVOController']]],
+  ['unobserve_3ainfo_3a',['unobserve:info:',['../interface___f_b_k_v_o_shared_controller.html#a86296472b759a5957e40bfe799e8073e',1,'_FBKVOSharedController']]],
+  ['unobserve_3ainfos_3a',['unobserve:infos:',['../interface___f_b_k_v_o_shared_controller.html#ac4ddda4d74732273dfe84a8edbbccfd0',1,'_FBKVOSharedController']]],
+  ['unobserve_3akeypath_3a',['unobserve:keyPath:',['../interface_f_b_k_v_o_controller.html#ac7362b1d5f39932948e4de66aba5cca9',1,'FBKVOController']]],
   ['unobserveall',['unobserveAll',['../interface_f_b_k_v_o_controller.html#a68986621c451e29b46b6b8eda62e188e',1,'FBKVOController']]]
 ];
--- doc/search/properties_0.js
+++ doc/search/properties_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['kvocontroller',['KVOController',['../category_n_s_object_07_f_b_k_v_o_controller_08.html#a1b8c562cef11b9d87f803d7721b5bd38',1,'NSObject(FBKVOController)']]]
+  ['kvocontroller',['KVOController',['../category_n_s_object_07_f_b_k_v_o_controller_08.html#aa95ad60ae38e9a30b495e54faedeed23',1,'NSObject(FBKVOController)']]]
 ];
