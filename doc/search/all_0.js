4f0e5d356660f2dabc7d11ea31c1456a8c7059aa
Tong Guo <Tong-G@outlook.com>
2014-11-14 14:16:36 +0800

    ADDED doc.

---
+++ doc/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['_5ffbkvoinfo',['_FBKVOInfo',['../interface___f_b_k_v_o_info.html',1,'']]],
+  ['_5ffbkvosharedcontroller',['_FBKVOSharedController',['../interface___f_b_k_v_o_shared_controller.html',1,'']]]
+];
---
+++ doc/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['controllerwithobserver_3a',['controllerWithObserver:',['../interface_f_b_k_v_o_controller.html#ab8b4b15ecd54384a02f19db9fc284176',1,'FBKVOController']]]
+];
