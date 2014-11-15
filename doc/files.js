d147c8a57cedd279cf3f049a878ac75d909f149b
Tong Guo <Tong-G@outlook.com>
2014-11-15 17:59:47 +0800

    Temporarily DELETED the documentation for merging to 'master' branch.

--- doc/files.js
+++
@@ -1,4 +0,0 @@
-var files =
-[
-    [ "FBKVOController", "dir_7ec7c9feece6cdff795a81eb0f2d1230.html", "dir_7ec7c9feece6cdff795a81eb0f2d1230" ]
-];
--- doc/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['fbkvocontroller',['FBKVOController',['../d7/d45/interface_f_b_k_v_o_controller.html',1,'']]]
-];
