85e8cddcc600e01cea0ac9e9d9a8bd0b83301d4d
Tong Guo <Tong-G@outlook.com>
2014-11-16 02:34:18 +0800

    UPDATED the documentation.

--- FBKVOController/FBKVOController.h
+++ FBKVOController/FBKVOController.h
@@ -68,7 +68,12 @@ typedef void (^FBKVONotificationBlock)(NSString* keyPath, id observer, id object
  @param keyPath The key path to observe.
  @param options The NSKeyValueObservingOptions to use for observation.
  @param action The observer selector called on key-value change.
- @discussion On key-value change, the observer's action selector is called. The selector provided should take the form of -propertyDidChange, -propertyDidChange: or -propertyDidChange:object:, where optional parameters delivered will be KVO change dictionary and object observed. Observing nil or observing an already observed object's key path results in no operation.
+ @discussion On key-value change, the observer's action selector is called. The <i>action</i> should have the following signature: 
+    <p><code>- ( void ) observedPropertiesChange: ( NSDictionary* )change
+                                         keyPath: ( NSString* )keyPath
+                                          object: ( id )object
+    </code></p>
+    <p>Observing nil or observing an already observed object's key path results in no operation. </p>
  */
 - (void)observe:(id)object keyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options action:(SEL)action;
 
@@ -99,7 +104,12 @@ typedef void (^FBKVONotificationBlock)(NSString* keyPath, id observer, id object
  @param keyPaths The key paths to observe.
  @param options The NSKeyValueObservingOptions to use for observation.
  @param action The observer selector called on key-value change.
- @discussion On key-value change, the observer's action selector is called. The selector provided should take the form of -propertyDidChange, -propertyDidChange: or -propertyDidChange:object:, where optional parameters delivered will be KVO change dictionary and object observed. Observing nil or observing an already observed object's key path results in no operation.
+ @discussion On key-value change, the observer's action selector is called. The <i>action</i> should have the following signature: 
+    <p><code>- ( void ) observedPropertiesChange: ( NSDictionary* )change
+                                         keyPath: ( NSString* )keyPath
+                                          object: ( id )object
+    </code></p>
+    <p>Observing nil or observing an already observed object's key path results in no operation. </p>
  */
 - (void)observe:(id)object keyPaths:(NSArray *)keyPaths options:(NSKeyValueObservingOptions)options action:(SEL)action;
 
